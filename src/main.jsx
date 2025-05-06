7b8139735a460bfded28e0c31cc847e0d70e039e
ArjonD <bhm527@myy.haaga-helia.fi>
2025-05-06 19:14:29 +0300

    uus yritys

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import Customerlist from './Components/Customers/Customerlist.jsx'
 import Traininglist from './Components/Trainings/Traininglist.jsx'
 import Home from './Components/Home.jsx'
 import Calendar from './Components/Calendar/Calendar.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "Customerlist",
-        element: <Customerlist />
-      },
-      {
-        path: "Traininglist",
-        element: <Traininglist />
-      },
-      {
-        path: "Calendar",
-        element: <Calendar />
-      }
-    ]
-  }
-]);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="Customerlist" element={<Customerlist />} />
+          <Route path="Traininglist" element={<Traininglist />} />
+          <Route path="Calendar" element={<Calendar />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
